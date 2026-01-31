61036699cd4eac11aaa93c3d95add89a6fd5c6c3
adityabajra <adityabajracharya18@gmail.com>
2026-01-31 16:19:07 +0545

    adding js

---
+++ js/data.js
@@ -0,0 +1,6 @@
+var groceryItems = [
+  { id: "1", name: "butter", completed: false },
+  { id: "2", name: "buns", completed: true },
+  { id: "3", name: "sauce", completed: false },
+  { id: "4", name: "honey", completed: false },
+];
